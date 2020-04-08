118d1ba1a506240fcbb668caf2a61ec72188fe6a
Alexis-Dk <alexeydruzik@inbox.ru>
2020-04-08 10:58:35 +0300

    Refactored example11.

--- src/main/java/example11/Client.java
+++ src/main/java/example11/Client.java
@@ -1,9 +1,8 @@
 package example11;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * @author Alexey Druzik on 4/6/2020
@@ -11,15 +10,19 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost", 45000);
-        OutputStream out = socket.getOutputStream();
-        InputStream in = socket.getInputStream();
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        socket.getOutputStream().write(next.getBytes());
+        socket.close();
+        //OutputStream out = socket.getOutputStream();
+        //InputStream in = socket.getInputStream();
 
-        out.write("hello".getBytes()); //Blocking call
+        //out.write("hello".getBytes()); //Blocking call
 
-        byte[] data = new byte[1024];
-        while (in.read(data) != -1) { //Blocking call
-        System.out.println("Client:" + data);
+        //byte[] data = new byte[1024];
+        //while (in.read(data) != -1) { //Blocking call
+        //System.out.println("Client:" + data);
         //handle(data);
     }
-}
+
 }
